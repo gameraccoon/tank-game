bfa5516aa88a9b35aca780f60aab29c698485f0a
Pavel Grebnev <ruwhynot@gmail.com>
2022-03-09 20:11:33 +0100

    Basic implementation of UniqueAny + unit tests

---
+++ src/Base/Types/ComplexTypes/UniqueAny.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <memory>
+
+/**
+ * @brief Class that implements non-copyable version of std::any
+ *
+ * It has pretty limited implementation, add new functionalily when needed
+ */
+class UniqueAny
+{
+public:
+	UniqueAny() noexcept = default;
+	~UniqueAny() = default;
+	UniqueAny(UniqueAny&) = delete;
+	UniqueAny(UniqueAny&& other) noexcept
+		: mData(std::move(other.mData))
+	{
+	}
+
+	UniqueAny& operator=(UniqueAny&) = delete;
+	UniqueAny& operator=(UniqueAny&& other) {
+		mData = std::move(other.mData);
+		return *this;
+	}
+
+	template <typename T, typename... Args>
+	static UniqueAny Create(Args&&... args)
+	{
+		UniqueAny newAny;
+		newAny.mData = std::make_unique<TypeStoredData<std::decay_t<T>>>(std::forward<Args>(args)...);
+		return newAny;
+	}
+
+	template <typename T>
+	T* cast() noexcept
+	{
+		if (mData && dynamic_cast<TypeStoredData<T>*>(mData.get()) != nullptr)
+		{
+			return &static_cast<TypeStoredData<T>*>(mData.get())->data;
+		}
+		return nullptr;
+	}
+
+	template <typename T>
+	const T* cast() const noexcept
+	{
+		if (mData && dynamic_cast<TypeStoredData<T>*>(mData.get()) != nullptr)
+		{
+			return &static_cast<TypeStoredData<T>*>(mData.get())->data;
+		}
+		return nullptr;
+	}
+
+private:
+	struct AbstractStoredData
+	{
+		virtual ~AbstractStoredData() = default;
+		virtual const std::type_info& getType() const noexcept = 0;
+	};
+
+	template <typename T>
+	struct TypeStoredData final : public AbstractStoredData
+	{
+
+		template <typename... Args>
+		TypeStoredData(Args&&... args)
+			: data(std::forward<Args>(args)...)
+		{}
+
+		const std::type_info& getType() const noexcept override
+		{
+			return typeid(T);
+		}
+
+		T data;
+	};
+
+	std::unique_ptr<AbstractStoredData> mData;
+};
---
+++ src/UnitTests/Tests/TestUniqueAny.cpp
@@ -0,0 +1,64 @@
+#include "Base/precomp.h"
+
+#include <gtest/gtest.h>
+
+#include <type_traits>
+#include <vector>
+
+#include "Base/Types/ComplexTypes/UniqueAny.h"
+
+TEST(UniqueAny, DefaultConstructed)
+{
+	UniqueAny any;
+	EXPECT_EQ(nullptr, any.cast<int>());
+	EXPECT_EQ(nullptr, any.cast<std::vector<int>>());
+}
+
+TEST(UniqueAny, MoveCopyConstructed)
+{
+	{
+		UniqueAny any{UniqueAny::Create<int>(10)};
+		ASSERT_NE(nullptr, any.cast<int>());
+		EXPECT_EQ(nullptr, any.cast<float>());
+		EXPECT_EQ(10, *any.cast<int>());
+	}
+
+	{
+		// use vector constuctor that takes size and default value
+		UniqueAny any{UniqueAny::Create<std::vector<int>>(3, 2)};
+		ASSERT_NE(nullptr, any.cast<std::vector<int>>());
+		EXPECT_EQ(nullptr, any.cast<int>());
+		EXPECT_EQ(std::vector<int>({2, 2, 2}), *any.cast<std::vector<int>>());
+	}
+}
+
+TEST(UniqueAny, MoveAssignConstructed)
+{
+	UniqueAny any;
+
+	any = UniqueAny::Create<int>(20);
+	ASSERT_NE(nullptr, any.cast<int>());
+	EXPECT_EQ(nullptr, any.cast<std::vector<int>>());
+	EXPECT_EQ(20, *any.cast<int>());
+
+	// use vector constuctor that takes size and default value
+	any = UniqueAny::Create<std::vector<int>>(3, 2);
+	ASSERT_NE(nullptr, any.cast<std::vector<int>>());
+	EXPECT_EQ(nullptr, any.cast<int>());
+	EXPECT_EQ(std::vector<int>({2, 2, 2}), *any.cast<std::vector<int>>());
+}
+
+
+TEST(UniqueAny, CastFromConst)
+{
+	const UniqueAny any = UniqueAny::Create<int>(2);
+
+	auto value = any.cast<int>();
+
+	static_assert(!std::is_const_v<decltype(*value)>, "UniqueAny::cast called on const object should return pointer to const");
+	ASSERT_NE(nullptr, value);
+	EXPECT_EQ(2, *value);
+}
+
+static_assert(!std::is_copy_constructible_v<UniqueAny>, "UniqueAny should not be copy constructible");
+static_assert(!std::is_copy_assignable_v<UniqueAny>, "UniqueAny should not be copy assignable");
