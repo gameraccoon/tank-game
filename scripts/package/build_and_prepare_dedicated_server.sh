ef0d8e21cad2115ab3803024f6047b0ad6c50965
Pavel Grebnev <ruwhynot@gmail.com>
2022-08-13 22:23:20 +0200

    Build and prepare dedicated server for linux on Github actions

---
+++ scripts/package/build_and_prepare_dedicated_server.sh
@@ -0,0 +1,14 @@
+mkdir build/dedicated_server -p
+pushd build/dedicated_server
+	cmake ../.. -DDEDICATED_SERVER=ON -DBUILD_UNIT_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
+	cmake --build .
+popd
+
+mkdir package/dedicated_server -p
+cp ./bin/DedicatedServer package/dedicated_server/
+cp -rL resources package/dedicated_server/
+rm -r package/dedicated_server/resources/animations
+rm -r package/dedicated_server/resources/atlas
+rm -r package/dedicated_server/resources/fonts
+rm -r package/dedicated_server/resources/prefabs
+rm -r package/dedicated_server/resources/textures
