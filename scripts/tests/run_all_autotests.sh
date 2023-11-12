11e5a5277dd792dfd370aafcd585a37edbd23098
Pavel Grebnev <ruwhynot@gmail.com>
2023-11-12 13:59:00 +0100

    Make script that runs all autotests output which autotest is being currently run

--- scripts/tests/run_all_autotests.sh
+++ scripts/tests/run_all_autotests.sh
@@ -3,6 +3,7 @@
 cases_list=$(./bin/AutoTests --list)
 
 while IFS= read -r line; do
-    ./bin/AutoTests --case $line "$@"
+	echo Run test case $line
+	./bin/AutoTests --case $line "$@"
 done <<< "$cases_list"
 
