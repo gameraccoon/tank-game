48a9d0edfefa1493ab0f82ed0a8c83bf5c3b820e
Pavel Grebnev <ruwhynot@gmail.com>
2023-12-04 11:17:36 +0100

    Added SimpleBitset

---
+++ src/Base/Types/ComplexTypes/SimpleBitset.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <cstddef>
+
+template<size_t BitCount>
+struct BitsetTraits
+{
+	constexpr static size_t ByteCount = BitCount / 8 + (BitCount % 8 == 0 ? 0 : 1);
+};
+
+/**
+ * Simple bitset that can be serialized and deserialized
+ */
+template<size_t BitCount>
+class SimpleBitset
+{
+public:
+	SimpleBitset() = default;
+	SimpleBitset(const SimpleBitset&) = default;
+	SimpleBitset(SimpleBitset&&) = default;
+	SimpleBitset& operator=(const SimpleBitset&) = default;
+	SimpleBitset& operator=(SimpleBitset&&) = default;
+	~SimpleBitset() = default;
+
+	size_t getBitCount() const noexcept { return BitCount; }
+
+	void set(size_t index, bool isSet) noexcept
+	{
+		setInner(index, isSet);
+	}
+
+	bool get(size_t index) const noexcept
+	{
+		return getInner(index);
+	}
+
+	void clear() noexcept
+	{
+		for (size_t i = 0; i < ByteCount; ++i)
+		{
+			mBitset[i] = std::byte(0);
+		}
+	}
+
+	void intersect(const SimpleBitset& other) noexcept
+	{
+		for (size_t i = 0; i < ByteCount; ++i)
+		{
+			mBitset[i] &= other.mBitset[i];
+		}
+	}
+
+	void unite(const SimpleBitset& other) noexcept
+	{
+		for (size_t i = 0; i < ByteCount; ++i)
+		{
+			mBitset[i] |= other.mBitset[i];
+		}
+	}
+
+	void invert() noexcept
+	{
+		for (size_t i = 0; i < ByteCount; ++i)
+		{
+			mBitset[i] = ~mBitset[i];
+		}
+	}
+
+	size_t getByteCount() const noexcept { return ByteCount; }
+	const std::byte* getRawData() const noexcept { return mBitset; }
+
+	void setRawData(const std::byte* data) noexcept
+	{
+		for (size_t i = 0; i < ByteCount; ++i)
+		{
+			mBitset[i] = data[i];
+		}
+	}
+
+private:
+	void setInner(size_t index, bool isSet)
+	{
+		AssertFatal(index < BitCount, "Invalid index %d, max is %d", index, BitCount);
+		const size_t byteIndex = index / 8;
+		const size_t bitIndex = index % 8;
+		if (isSet)
+		{
+			mBitset[byteIndex] |= std::byte(1 << bitIndex);
+		}
+		else
+		{
+			mBitset[byteIndex] &= ~std::byte(1 << bitIndex);
+		}
+	}
+
+	bool getInner(size_t index) const noexcept
+	{
+		AssertFatal(index < BitCount, "Invalid index %d, max is %d", index, BitCount);
+		const size_t byteIndex = index / 8;
+		const size_t bitIndex = index % 8;
+		return (mBitset[byteIndex] & std::byte(1 << bitIndex)) != std::byte(0);
+	}
+
+private:
+	constexpr static size_t ByteCount = BitsetTraits<BitCount>::ByteCount;
+	std::byte mBitset[ByteCount] = { std::byte(0) };
+};
---
+++ src/UnitTests/Tests/TestSimpleBitset.cpp
@@ -0,0 +1,241 @@
+#include "Base/precomp.h"
+
+#include <gtest/gtest.h>
+
+#include "Base/Types/ComplexTypes/SimpleBitset.h"
+
+TEST(BitsetTraits, BitsetTraits_ByteCount_ReasonableAmountOfBytes)
+{
+	EXPECT_EQ(static_cast<size_t>(1), BitsetTraits<1>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(1), BitsetTraits<8>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(2), BitsetTraits<9>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(2), BitsetTraits<16>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(3), BitsetTraits<17>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(3), BitsetTraits<24>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(4), BitsetTraits<25>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(4), BitsetTraits<32>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(10), BitsetTraits<80>::ByteCount);
+	EXPECT_EQ(static_cast<size_t>(11), BitsetTraits<81>::ByteCount);
+}
+
+TEST(SimpleBitset, BitsetWithSize_GetBitCount_ReturnsExpectedValue)
+{
+	SimpleBitset<8> bitset;
+	EXPECT_EQ(static_cast<size_t>(8), bitset.getBitCount());
+
+	SimpleBitset<9> bitset2;
+	EXPECT_EQ(static_cast<size_t>(9), bitset2.getBitCount());
+
+	SimpleBitset<16> bitset3;
+	EXPECT_EQ(static_cast<size_t>(16), bitset3.getBitCount());
+
+	SimpleBitset<17> bitset4;
+	EXPECT_EQ(static_cast<size_t>(17), bitset4.getBitCount());
+
+	SimpleBitset<24> bitset5;
+	EXPECT_EQ(static_cast<size_t>(24), bitset5.getBitCount());
+
+	SimpleBitset<25> bitset6;
+	EXPECT_EQ(static_cast<size_t>(25), bitset6.getBitCount());
+
+	SimpleBitset<32> bitset7;
+	EXPECT_EQ(static_cast<size_t>(32), bitset7.getBitCount());
+
+	SimpleBitset<80> bitset8;
+	EXPECT_EQ(static_cast<size_t>(80), bitset8.getBitCount());
+
+	SimpleBitset<81> bitset9;
+	EXPECT_EQ(static_cast<size_t>(81), bitset9.getBitCount());
+}
+
+TEST(SimpleBitset, BitsetWithValues_CallGet_ReturnsExpectedValues)
+{
+	SimpleBitset<8> bitset;
+	bitset.set(0, true);
+	bitset.set(1, false);
+	bitset.set(2, true);
+	bitset.set(3, false);
+	bitset.set(4, true);
+	bitset.set(5, false);
+	bitset.set(6, true);
+	bitset.set(7, false);
+
+	EXPECT_TRUE(bitset.get(0));
+	EXPECT_FALSE(bitset.get(1));
+	EXPECT_TRUE(bitset.get(2));
+	EXPECT_FALSE(bitset.get(3));
+	EXPECT_TRUE(bitset.get(4));
+	EXPECT_FALSE(bitset.get(5));
+	EXPECT_TRUE(bitset.get(6));
+	EXPECT_FALSE(bitset.get(7));
+}
+
+TEST(SimpleBitset, BitsetWithValues_Clear_ClearsAllValues)
+{
+	SimpleBitset<8> bitset;
+	bitset.set(0, true);
+	bitset.set(1, false);
+	bitset.set(2, true);
+	bitset.set(3, false);
+	bitset.set(4, true);
+	bitset.set(5, false);
+	bitset.set(6, true);
+	bitset.set(7, false);
+
+	bitset.clear();
+
+	EXPECT_FALSE(bitset.get(0));
+	EXPECT_FALSE(bitset.get(1));
+	EXPECT_FALSE(bitset.get(2));
+	EXPECT_FALSE(bitset.get(3));
+	EXPECT_FALSE(bitset.get(4));
+	EXPECT_FALSE(bitset.get(5));
+	EXPECT_FALSE(bitset.get(6));
+	EXPECT_FALSE(bitset.get(7));
+}
+
+TEST(SimpleBitset, TwoBitsetsWithValues_Intersect_OnlyCommonValuesAreSet)
+{
+	SimpleBitset<4> bitset1;
+	bitset1.set(0, false);
+	bitset1.set(1, true);
+	bitset1.set(2, false);
+	bitset1.set(3, true);
+
+	SimpleBitset<4> bitset2;
+	bitset2.set(0, false);
+	bitset2.set(1, false);
+	bitset2.set(2, true);
+	bitset2.set(3, true);
+
+	bitset1.intersect(bitset2);
+
+	EXPECT_FALSE(bitset1.get(0));
+	EXPECT_FALSE(bitset1.get(1));
+	EXPECT_FALSE(bitset1.get(2));
+	EXPECT_TRUE(bitset1.get(3));
+}
+
+TEST(SimpleBitset, TwoBitsetsWithValues_Unite_AllValuesAreSet)
+{
+	SimpleBitset<4> bitset1;
+	bitset1.set(0, false);
+	bitset1.set(1, true);
+	bitset1.set(2, false);
+	bitset1.set(3, true);
+
+	SimpleBitset<4> bitset2;
+	bitset2.set(0, false);
+	bitset2.set(1, false);
+	bitset2.set(2, true);
+	bitset2.set(3, true);
+
+	bitset1.unite(bitset2);
+
+	EXPECT_FALSE(bitset1.get(0));
+	EXPECT_TRUE(bitset1.get(1));
+	EXPECT_TRUE(bitset1.get(2));
+	EXPECT_TRUE(bitset1.get(3));
+}
+
+TEST(SimpleBitset, BitsetWithValues_Invert_InvertsAllValues)
+{
+	SimpleBitset<8> bitset;
+	bitset.set(0, true);
+	bitset.set(1, false);
+	bitset.set(2, true);
+	bitset.set(3, false);
+	bitset.set(4, true);
+	bitset.set(5, false);
+	bitset.set(6, true);
+	bitset.set(7, false);
+
+	bitset.invert();
+
+	EXPECT_FALSE(bitset.get(0));
+	EXPECT_TRUE(bitset.get(1));
+	EXPECT_FALSE(bitset.get(2));
+	EXPECT_TRUE(bitset.get(3));
+	EXPECT_FALSE(bitset.get(4));
+	EXPECT_TRUE(bitset.get(5));
+	EXPECT_FALSE(bitset.get(6));
+	EXPECT_TRUE(bitset.get(7));
+}
+
+TEST(SimpleBitset, BitsetWithValues_GetRawDataAndThenSetRawData_ProducesSameValues)
+{
+	std::array<std::byte, BitsetTraits<8>::ByteCount> rawData;
+	{
+		SimpleBitset<8> bitset;
+		bitset.set(0, true);
+		bitset.set(1, false);
+		bitset.set(2, true);
+		bitset.set(3, false);
+		bitset.set(4, true);
+		bitset.set(5, false);
+		bitset.set(6, true);
+		bitset.set(7, false);
+
+		std::copy(bitset.getRawData(), bitset.getRawData() + BitsetTraits<8>::ByteCount, rawData.data());
+	}
+	SimpleBitset<8> bitset2;
+	bitset2.setRawData(rawData.data());
+
+	EXPECT_TRUE(bitset2.get(0));
+	EXPECT_FALSE(bitset2.get(1));
+	EXPECT_TRUE(bitset2.get(2));
+	EXPECT_FALSE(bitset2.get(3));
+	EXPECT_TRUE(bitset2.get(4));
+	EXPECT_FALSE(bitset2.get(5));
+	EXPECT_TRUE(bitset2.get(6));
+	EXPECT_FALSE(bitset2.get(7));
+}
+
+// make sure we don't depend on endianness, and this test doesn't fail on some platforms with the same data
+TEST(SimpleBitset, RawValues_CreateBitsetWithRawData_ProducesExpectedValues)
+{
+	std::array<std::byte, BitsetTraits<8>::ByteCount> rawData;
+	rawData[0] = std::byte(0xF7);
+
+	SimpleBitset<8> bitset;
+	bitset.setRawData(rawData.data());
+
+	EXPECT_TRUE(bitset.get(0));
+	EXPECT_TRUE(bitset.get(1));
+	EXPECT_TRUE(bitset.get(2));
+	EXPECT_FALSE(bitset.get(3));
+	EXPECT_TRUE(bitset.get(4));
+	EXPECT_TRUE(bitset.get(5));
+	EXPECT_TRUE(bitset.get(6));
+	EXPECT_TRUE(bitset.get(7));
+}
+
+TEST(SimpleBitset, BitsetWithSize_GetByteCount_ReturnsExpectedValue)
+{
+	SimpleBitset<8> bitset;
+	EXPECT_EQ(static_cast<size_t>(1), bitset.getByteCount());
+
+	SimpleBitset<9> bitset2;
+	EXPECT_EQ(static_cast<size_t>(2), bitset2.getByteCount());
+
+	SimpleBitset<16> bitset3;
+	EXPECT_EQ(static_cast<size_t>(2), bitset3.getByteCount());
+
+	SimpleBitset<17> bitset4;
+	EXPECT_EQ(static_cast<size_t>(3), bitset4.getByteCount());
+
+	SimpleBitset<24> bitset5;
+	EXPECT_EQ(static_cast<size_t>(3), bitset5.getByteCount());
+
+	SimpleBitset<25> bitset6;
+	EXPECT_EQ(static_cast<size_t>(4), bitset6.getByteCount());
+
+	SimpleBitset<32> bitset7;
+	EXPECT_EQ(static_cast<size_t>(4), bitset7.getByteCount());
+
+	SimpleBitset<80> bitset8;
+	EXPECT_EQ(static_cast<size_t>(10), bitset8.getByteCount());
+
+	SimpleBitset<81> bitset9;
+	EXPECT_EQ(static_cast<size_t>(11), bitset9.getByteCount());
+}
