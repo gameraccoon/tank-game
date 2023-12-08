62852363a7efe9583ee131dc91005807becd5106
Pavel Grebnev <ruwhynot@gmail.com>
2023-12-08 17:57:18 +0100

    Script that runs all autotests outputs the list of failed tests at the end of the run

--- scripts/tests/run_all_autotests.sh
+++ scripts/tests/run_all_autotests.sh
@@ -2,8 +2,24 @@
 
 cases_list=$(./bin/AutoTests --list)
 
-while IFS= read -r line; do
-	echo Run test case $line
-	./bin/AutoTests --case $line "$@"
+failed_cases=()
+
+while IFS= read -r case_name; do
+	echo Run test case $case_name
+	./bin/AutoTests --case $case_name "$@"
+	if [ $? -ne 0 ]; then
+		failed_cases+=($case_name)
+	fi
 done <<< "$cases_list"
 
+if [ ${#failed_cases[@]} -ne 0 ]; then
+	echo ""
+
+	for case_name in ${failed_cases[@]}; do
+		echo "Test case '$case_name' failed"
+	done
+
+	exit 1
+else
+	echo "All test cases successfully completed"
+fi
