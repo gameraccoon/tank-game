812d2bb1e004e953f56b8d28c3ace17c11cbef3a
Pavel Grebnev <ruwhynot@gmail.com>
2024-02-18 17:56:35 +0100

    Added TCP client implementation

---
+++ src/Utils/Network/TcpClient.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <optional>
+#include <memory>
+
+class TcpClient {
+public:
+	TcpClient();
+	TcpClient(const TcpClient&) = delete;
+	TcpClient(TcpClient&&) noexcept = delete;
+	TcpClient& operator=(const TcpClient&) = delete;
+	TcpClient& operator=(TcpClient&&) noexcept = delete;
+	~TcpClient();
+
+	// true if connection was successful, false otherwise
+	bool connectToServer(const std::string& serverIp, const std::string& serverPort);
+	void sendMessage(const std::string& message);
+	std::optional<std::string> receiveMessage();
+
+private:
+	struct Impl;
+	std::unique_ptr<Impl> mPimpl;
+
+	static constexpr int MAX_DATA_SIZE = 100;
+};
--- src/Utils/World/GameDataLoader.h
+++ src/Utils/World/GameDataLoader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <filesystem>
 
 #include "GameData/EcsDefinitions.h"
 
