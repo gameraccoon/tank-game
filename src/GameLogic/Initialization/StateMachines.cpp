d52bbdcbeac494c66d623aed6b845e8a82cbe7ab
Pavel Grebnev <ruwhynot@gmail.com>
2024-09-01 13:05:18 +0200

    Moved state machines initialization to Lua

--- src/GameLogic/Initialization/StateMachines.cpp
+++ src/GameLogic/Initialization/StateMachines.cpp
@@ -2,49 +2,116 @@
 
 #include "GameLogic/Initialization/StateMachines.h"
 
+#include "GameUtils/Scripting/LuaFunctionCall.h"
+#include "GameUtils/Scripting/LuaInstance.h"
+
 namespace StateMachines
 {
 	static void RegisterCharacterSM(FSM::StateMachine<CharacterState, CharacterStateBlackboardKeys>& sm)
 	{
 		using FSMType = FSM::StateMachine<CharacterState, CharacterStateBlackboardKeys>;
+
+		LuaInstance luaInstance(LuaInstance::OpenStandardLibs::Yes);
+
+		const AbsoluteResourcePath scriptPath(std::filesystem::current_path(), RelativeResourcePath("resources/scripts/config/state_machines.lua"));
+		const LuaExecResult execResult = luaInstance.execScriptFromFile(scriptPath);
+		if (execResult.statusCode != 0)
 		{
-			FSMType::StateLinkRules rules;
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Walk, CharacterStateBlackboardKeys::TryingToMove, true);
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Shoot, CharacterStateBlackboardKeys::TryingToShoot, true);
-			sm.addState(CharacterState::Idle, std::move(rules));
+			ReportErrorRelease("Failed to execute state_machines.lua: %s", execResult.errorMessage.c_str());
+			return;
 		}
+		lua_State& luaState = luaInstance.getLuaState();
+
+		LuaInternal::GetGlobal(luaState, "characterFsm");
+		if (!LuaInternal::IsTable(luaState, 0))
 		{
-			FSMType::StateLinkRules rules;
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Idle, CharacterStateBlackboardKeys::TryingToMove, false);
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::WalkAndShoot, CharacterStateBlackboardKeys::TryingToShoot, true);
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Run, CharacterStateBlackboardKeys::ReadyToRun, true);
-			sm.addState(CharacterState::Walk, std::move(rules));
+			ReportErrorRelease("Failed to get characterFsm table from state_machines.lua");
+			return;
 		}
-		{
+
+		// for each state
+		// ReSharper disable once CppDeclarationHidesUncapturedLocal
+		LuaType::IterateOverTable(luaState, [&sm](lua_State& luaState) {
+			CharacterState stateId;
+			if (const std::optional<CharacterState> name = LuaType::ReadValue<CharacterState>(luaState, LuaInternal::STACK_TOP - 1))
+			{
+				stateId = *name;
+			}
+			else
+			{
+				ReportErrorRelease("Failed to get state name from a state table in characterFsm table in state_machines.lua");
+				return;
+			}
+
+			if (!LuaInternal::IsTable(luaState, LuaInternal::STACK_TOP))
+			{
+				ReportErrorRelease("Failed to get a state table from characterFsm table in state_machines.lua");
+				return;
+			}
+
+			// for each link
 			FSMType::StateLinkRules rules;
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::WalkAndShoot, CharacterStateBlackboardKeys::TryingToMove, true);
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Idle, CharacterStateBlackboardKeys::TryingToShoot, false);
-			sm.addState(CharacterState::Shoot, std::move(rules));
-		}
-		{
-			FSMType::StateLinkRules rules;
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Shoot, CharacterStateBlackboardKeys::TryingToMove, false);
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Walk, CharacterStateBlackboardKeys::TryingToShoot, false);
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Run, CharacterStateBlackboardKeys::ReadyToRun, true);
-			sm.addState(CharacterState::WalkAndShoot, std::move(rules));
-		}
-		{
-			FSMType::StateLinkRules rules;
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Walk, CharacterStateBlackboardKeys::ReadyToRun, false);
-			rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(CharacterState::Idle, CharacterStateBlackboardKeys::TryingToMove, false);
-			sm.addState(CharacterState::Run, std::move(rules));
-		}
+			LuaType::IterateOverTable(luaState, [&rules](lua_State& luaState) {
+				if (!LuaInternal::IsTable(luaState, LuaInternal::STACK_TOP))
+				{
+					ReportErrorRelease("Failed to get a link table from a state table in characterFsm table in state_machines.lua");
+					return;
+				}
+
+				std::string linkType;
+				if (std::optional<std::string> type = LuaType::ReadField<std::string>(luaState, "type"))
+				{
+					linkType = std::move(*type);
+				}
+				else
+				{
+					ReportErrorRelease("Failed to get link type from a link table in a state table in characterFsm table in state_machines.lua");
+					return;
+					;
+				}
+
+				if (linkType == "VariableEqualLink")
+				{
+					const std::optional<CharacterState> targetState = LuaType::ReadField<CharacterState>(luaState, "targetState");
+					if (!targetState)
+					{
+						ReportErrorRelease("Failed to get targetState from a VariableEqualLink table in a state table in characterFsm table in state_machines.lua");
+						return;
+						;
+					}
+
+					const std::optional<CharacterStateBlackboardKeys> variableName = LuaType::ReadField<CharacterStateBlackboardKeys>(luaState, "variableName");
+					if (!variableName)
+					{
+						ReportErrorRelease("Failed to get variableName from a VariableEqualLink table in a state table in characterFsm table in state_machines.lua");
+						return;
+					}
+
+					const std::optional<bool> value = LuaType::ReadField<bool>(luaState, "value");
+					if (!value)
+					{
+						ReportErrorRelease("Failed to get value from a VariableEqualLink table in a state table in characterFsm table in state_machines.lua");
+						return;
+					}
+
+					rules.emplaceLink<FSM::LinkRules::VariableEqualLink, bool>(*targetState, *variableName, *value);
+				}
+				else
+				{
+					ReportErrorRelease("Unknown link type in a link table in a state table in characterFsm table in state_machines.lua");
+				}
+			});
+
+			sm.addState(stateId, std::move(rules));
+		});
+		LuaInternal::Pop(luaState); // pop characterFsm table
 	}
 
 	void RegisterStateMachines(StateMachineComponent* stateMachine)
 	{
 		if (!stateMachine)
 		{
+			ReportError("StateMachineComponent is nullptr when registering state machines");
 			return;
 		}
 
