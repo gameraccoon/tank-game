6fad90a86fee20e7220cf4ae00d4a10df3b2ce2b
Pavel Grebnev <ruwhynot@gmail.com>
2024-09-21 10:56:32 +0200

    Added test for executing AngelScript script from memory

--- src/UnitTests/Tests/TestAngelScriptEngine.cpp
+++ src/UnitTests/Tests/TestAngelScriptEngine.cpp
@@ -12,7 +12,7 @@
 
 #include "UnitTests/TestAssertHelper.h"
 
-namespace AngelScriptEngineInternal
+namespace AngelScriptEngineTestInternal
 {
 	static int gCallCount = 0;
 	void testFunction(const std::string& message)
@@ -22,9 +22,56 @@ namespace AngelScriptEngineInternal
 	}
 }
 
+TEST(AngelScriptEngine, Instance_ExecuteValidScriptFromString_StatusCodeIsZero)
+{
+	using namespace AngelScriptEngineTestInternal;
+
+	const int expectedCallCount = gCallCount + 1;
+
+	AngelScriptGlobalEngine engine;
+
+	int r = AngelScriptGlobalEngine::GetEngine().RegisterGlobalFunction("void testFunction(const string& in)", WRAP_FN(testFunction), asCALL_GENERIC);
+	Assert(r >= 0, "Failed to register function");
+
+	CScriptBuilder builder;
+	r = builder.StartNewModule(&AngelScriptGlobalEngine::GetEngine(), "MyModule");
+	if (r < 0)
+	{
+		ReportFatalError("Unrecoverable error while starting a new module. Possibly out of memory.");
+		return;
+	}
+	r = builder.AddSectionFromMemory("MySection", "void main() { testFunction(\"Test text\"); }");
+	if (r < 0)
+	{
+		ReportError("Please correct the errors in the script and try again.");
+		return;
+	}
+	r = builder.BuildModule();
+	if (r < 0)
+	{
+		ReportError("Please correct the errors in the script and try again.");
+		return;
+	}
+
+	// find the function that is to be called
+	const asIScriptModule* mod = AngelScriptGlobalEngine::GetEngine().GetModule("MyModule");
+	asIScriptFunction* func = mod->GetFunctionByDecl("void main()");
+	if (func == nullptr)
+	{
+		ReportError("The script must have the function 'void main()'. Please add it and try again.");
+		return;
+	}
+
+	AngelScriptContext ctx;
+
+	ctx.ExecuteFunction(func);
+
+	EXPECT_EQ(expectedCallCount, gCallCount);
+}
+
 TEST(AngelScriptEngine, Instance_ExecuteValidScriptFromFile_StatusCodeIsZero)
 {
-	using namespace AngelScriptEngineInternal;
+	using namespace AngelScriptEngineTestInternal;
 
 	const int expectedCallCount = gCallCount + 1;
 
@@ -53,7 +100,7 @@ TEST(AngelScriptEngine, Instance_ExecuteValidScriptFromFile_StatusCodeIsZero)
 		return;
 	}
 
-	// Find the function that is to be called.
+	// find the function that is to be called
 	const asIScriptModule* mod = AngelScriptGlobalEngine::GetEngine().GetModule("MyModule");
 	asIScriptFunction* func = mod->GetFunctionByDecl("void main()");
 	if (func == nullptr)
@@ -129,7 +176,7 @@ TEST(AngelScriptEngine, Instance_ExecuteScriptFromFileThatRaisesException_Assert
 		return;
 	}
 
-	// Find the function that is to be called.
+	// find the function that is to be called
 	const asIScriptModule* mod = AngelScriptGlobalEngine::GetEngine().GetModule("MyModule");
 	asIScriptFunction* func = mod->GetFunctionByDecl("void main()");
 	if (func == nullptr)
